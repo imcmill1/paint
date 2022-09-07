7971da10137812ae46ec41f8a9ba79773bac378a
Ian <ian.mcmillan1@valpo.edu>
2022-09-07 18:51:15 -0500

    Version 0.1.0
    Implements Menu Bar with
    - File -> Open, Save, Save As, Close
    - No functionality present

--- src/main/java/com/paint/paint/Controller.java
+++ src/main/java/com/paint/paint/Controller.java
@@ -3,7 +3,7 @@ package com.paint.paint;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class fxml {
     @FXML
     private Label welcomeText;
 
