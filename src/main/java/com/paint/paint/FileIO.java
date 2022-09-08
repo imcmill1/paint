3b7ee56dd382178f65d65db76e2d308ae1c31288
Ian <ian.mcmillan1@valpo.edu>
2022-09-08 00:09:56 -0500

    Version 0.1.1
    File Changes:
    + Added - FileIO.java, a FileIO class to handle basic file i/o operations
    
    Retains previous functionality and adds:
    - Placeholder methods within FileIO for open, save, and saveAs
    - Limited file explorer functionality, file explorer can be opened when open button is pressed.

--- src/main/java/com/paint/paint/FileIO.java
+++
@@ -1,2 +0,0 @@
-package com.paint.paint;public class FileIO {
-}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.paint.paint {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.desktop;
 
 
     opens com.paint.paint to javafx.fxml;
