7c3ab8373b610c8841df2d4e3ef8919675d63123
Ian <ian.mcmillan1@valpo.edu>
2022-09-08 22:12:20 -0500

    Version 0.2.1
    Retains all previous functionality and adds:
    - Images opened on canvas now open to fit the size of the canvas.
    - Documented all code
    - Fixed GraphicsContext issue

--- src/main/java/com/paint/paint/Controller.java
+++ src/main/java/com/paint/paint/Controller.java
@@ -2,17 +2,50 @@ package com.paint.paint;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 
 import java.io.IOException;
+import java.io.File;
 
-public class Controller {
+public class Controller { //static controller class
+    //=====FXML LOADS=====//
+    @FXML private StackPane displayPane; //inject the stackpane for the display
+    @FXML
+    private Canvas baseCanvas; //injects canvas and creates graphics context variable.
+    private GraphicsContext gc;
+
+
+    //=====FILE IO METHODS=====//
+    //These are the methods from the fileIO static class. They have to be wrapped in a method here in the controller for them to be used
+    //in FXML. Most are one line commands.
+    @FXML
+    protected void open() throws IOException { //wrapped open() function
+        String selectedImg = fileIO.open(); //creates string selectedImg and assigns it to a call of fileIO.open()
+        Display.showImage(displayPane, baseCanvas, selectedImg); //passes that string as the path into showImage
+    }
 
     @FXML
-    protected void open() throws IOException { fileIO.open();}
-
-    @FXML
-    protected void save() { fileIO.save();}
+    protected void save() throws IOException { fileIO.save(baseCanvas);}
 
     @FXML
     protected void saveAs() { fileIO.saveAs();}
+
+    //=====DISPLAY METHODS=====//
+    //Methods from the Display class. Similar to above, these methods must be wrapped in a method inside the controller class.
+    //Note: one exception is that currently the showImage() method is not wrapped. Instead it is called directly from the Controller.open() method.
+    @FXML
+    protected void closeLast() {Display.closeLast(displayPane);}
+
+    @FXML
+    protected void closeAll() {Display.closeAll(displayPane);}
+
+    //=====INITIALIZE=====//
+    //placeholder initialize method for things not directly injected into FXML.
+    //Direct injection is preferred.
+    public void initialize() {
+        gc = baseCanvas.getGraphicsContext2D();
+    }
 }
